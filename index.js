b8239da2ab2ed8f4fe8a5cc0f909d29198caea8d
otsomikkila <otso.mikkila@hotmail.fi>
2024-12-16 21:09:47 +0200

    phonebook backend step 4

--- index.js
+++ index.js
@@ -47,6 +47,13 @@ app.get('/api/persons/:id', (request, response) => {
     }
 })
 
+app.delete('/api/persons/:id', (request, response) => {
+    const id = request.params.id
+    data = data.filter(n => n.id !== id)
+  
+    response.status(204).end()
+})
+
 const PORT = 3001
 app.listen(PORT)
 console.log(`Server running on port ${PORT}`)
