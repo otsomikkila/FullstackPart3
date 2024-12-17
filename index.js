b13f3b6553bb8a1efd0af3c561d73b6f36ff5d9e
otsomikkila <otso.mikkila@hotmail.fi>
2024-12-17 11:37:16 +0200

    phonebook backend step 9

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const app = express()
 app.use(express.json())
 const cors = require('cors')
 app.use(cors())
+app.use(express.static('dist'))
 
 app.use(morgan(function (tokens, req, res) {
   return [
