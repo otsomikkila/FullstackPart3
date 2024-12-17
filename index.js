225c5550fa4619fb417d5d0c8ff6ad29b1a0f424
otsomikkila <otso.mikkila@hotmail.fi>
2024-12-17 11:15:11 +0200

    added cors support

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express')
 const morgan = require('morgan')
 const app = express()
 app.use(express.json())
+const cors = require('cors')
+app.use(cors())
 
 app.use(morgan(function (tokens, req, res) {
   return [
